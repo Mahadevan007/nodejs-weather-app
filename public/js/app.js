2c0b98f3fb5b28326d33d51d1475eca2bb694199
Mahadevan007 <42490734+Mahadevan007@users.noreply.github.com>
2019-09-20 00:00:47 +0530

    Remove unnecessary console.log

--- public/js/app.js
+++ public/js/app.js
@@ -5,9 +5,6 @@ const weatherform = document.querySelector("form");
 const search = document.querySelector("input");
 
 const messageOne = document.querySelector("#message-1");
-
-const messageTwo = document.querySelector("#message-2");
-
 weatherform.addEventListener("submit", e => {
   e.preventDefault();
 
