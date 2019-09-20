8789a7dc0337d4afea4bb94cc30f506de0717a26
Mahadevan007 <42490734+Mahadevan007@users.noreply.github.com>
2019-09-20 09:38:26 +0530

    Variable error correction

--- public/js/app.js
+++ public/js/app.js
@@ -5,6 +5,7 @@ const weatherform = document.querySelector("form");
 const search = document.querySelector("input");
 
 const messageOne = document.querySelector("#message-1");
+const messageTwo = document.querySelector("#message-2");
 weatherform.addEventListener("submit", e => {
   e.preventDefault();
 
